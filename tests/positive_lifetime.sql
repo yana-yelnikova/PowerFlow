d8cc930e1a52794f73273732d936ed8ba889282c
Yana Yelnikova <yana.yelnikova@gmail.com>
2025-06-03 14:07:27 +0000

    schema and test commented

--- tests/positive_lifetime.sql
+++ tests/positive_lifetime.sql
@@ -1,3 +1,6 @@
+-- This query selects all records from the `ltv` model where the `lifetime` value (number of days between registration and purchase) is less than 0.
+-- Such cases likely indicate data issues, for example, if a purchase date is earlier than the user's registration date.
+-- This can be useful for debugging or cleaning the data before final ROI calculations.
 select * from {{ ref("ltv") }} as l 
 where 
         lifetime < 0
